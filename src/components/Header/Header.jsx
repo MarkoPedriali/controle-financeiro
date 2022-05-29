32e1fd3529c4b7d98caf57625c9966faae93cdcf
MarkoPedriali <marko.pedriali@hotmail.com>
2022-05-28 21:32:26 -0300

    Instalação do Sass + Estilização dos componentes Header e Main

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './output.css';
+import menu from "../../assets/menu.png";
+import user from "../../assets/user.png";
+
+function Header() {
+    return(
+        <div>
+        <header id="header">
+            <nav>
+                <img src={menu} className="nav-link icon" id="menu" alt="menu icon"></img>
+                <h1 className="nav-link">Controle Financeiro</h1>
+                <img src={user} className="nav-link icon" id="menu" alt="user login icon"></img>
+            </nav>
+        </header>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,33 @@
+import './output.css';
+
+function Main(){
+    return(
+        <main id="main">
+            <div className="transacao">
+                <form>
+                    <label for="tipo"><h2>Tipo de transação</h2></label>
+                    <select name="tipo" className="inputs">
+                        <option value="compra" className="options">Comprar</option>
+                        <option value="venda" className="options">Vender</option>
+                    </select>
+                    <label for="produto"><h4>Nome da mercadoria</h4></label>
+                    <input type="text" name="produto" className="inputs"></input>
+                    <label for="valor"><h4>Valor</h4></label>
+                    <input type="number" className="inputs"></input>
+                    <button>ADICIONAR</button>
+                </form>
+            </div>
+            <div className="extrato">
+                <h2>Extrato das transações</h2>
+                <div className="lista-produtos">
+                    <p>produto</p>
+                    <p>produto</p>
+                    <p>produto</p>
+                </div>
+                <h2 id="total">Total</h2>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
