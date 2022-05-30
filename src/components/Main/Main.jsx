b1ccc03b0691cbbacc180b8c57eca289ac3fac18
MarkoPedriali <marko.pedriali@hotmail.com>
2022-05-30 20:06:29 -0300

    Implementação da função de calcular extrato

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,24 +1,45 @@
 import { useState } from 'react';
 import './output.css';
 
-function Main(){
-
+function Main(){   
+    var[saldo,setSaldo] = useState(0);
     const[tipo, setTipo] = useState('compra');
     const[nome, setNome] = useState('');
     const[valor, setValor] = useState('');
 
-    var item = tipo + nome + valor;
+    const handleReset = () => {
+        Array.from(document.querySelectorAll("input")).forEach(
+          input => (input.value = "")
+        );
+    }
 
     const handleSubmit = async e => {
         e.preventDefault();
-        alert(item);
+
+        var item = {
+            "tipo":tipo,
+            "nome":nome,
+            "valor":valor
+        }
+       
+        handleReset();
+
+        extrato(item);
     };
 
+    const extrato = (item) =>{  
+        var tipo = item.tipo;
+        var valor = parseFloat(item.valor);
+        
+        tipo === "venda" ? setSaldo(saldo += valor) : setSaldo(saldo -= valor);
+        
+        console.log(saldo);
+    }
 
     return(
         <main id="main">
             <div className="transacao">
-                <form onSubmit={handleSubmit}>
+                <form id="forms" onSubmit={handleSubmit}>
                     <label for="tipo"><h4>Tipo de transação</h4></label>
                     <select name="tipo" className="inputs" onChange={e => setTipo(e.target.value)} required>
                         <option selected value="compra" className="options">Comprar</option>
